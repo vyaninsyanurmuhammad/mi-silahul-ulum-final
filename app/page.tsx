b6435de3122a6a47ea6b43a118c3cdd4b8e2b4f5
UC8096 <vyaninsya.nurmuhammad@gmail.com>
2023-11-12 20:16:07 +0700

    feat(view): implement NavigationBar to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import NavigationBar from './components/navigation-bar'
 
 export default function Home() {
   return (
     <>
+    <NavigationBar></NavigationBar>
       <div>
         Home
       </div>
