c193d48cb3cda2cfac12b99564015ae473154c11
UC8096 <vyaninsya.nurmuhammad@gmail.com>
2023-11-13 17:59:31 +0700

    feat(view): add initial about card at home page

---
+++ app/components/about-card-home.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@nextui-org/react"
+import React from 'react'
+import { ArrowSmallRightIcon } from '@heroicons/react/24/outline'
+
+export default function AboutCardHome() {
+    return (
+        <>
+            <div className="flex justify-center w-full px-16 mt-16">
+                <div className="flex flex-row gap-12 rounded-xl overflow-hidden bg-white w-full px-14 pt-14 shadow-xl">
+                    <div className="bg-green-300">
+                        <div className="w-60 h-60">Image</div>
+                    </div>
+                    <div className="flex flex-col justify-center gap-3">
+                        <div>
+                            <div className="text-xl font-semibold">Tentang Kami</div>
+                            <div className="text-xl font-semibold">Madrasah Ibtidaiyah Silahul Ulum</div>
+                        </div>
+                        <div>Descriptons</div>
+                        <Button className="w-fit rounded-full bg-aztec-500 text-white">Cek Sekarang <ArrowSmallRightIcon className="h-6 w-6 text-white"></ArrowSmallRightIcon> </Button>
+                    </div>
+
+                </div>
+            </div>
+
+        </>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import NavigationBar from './components/navigation-bar'
 import HeroHome from "./components/hero-home"
+import AboutCardHome from "./components/about-card-home"
 
 export default function Home() {
   return (
     <>
       <NavigationBar></NavigationBar>
       <HeroHome></HeroHome>
+      <AboutCardHome></AboutCardHome>
       <div>
         Home
       </div>
