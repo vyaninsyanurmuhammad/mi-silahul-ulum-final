e0a212dabb18aa77a47715494e1737f2bf43bc32
UC8096 <vyaninsya.nurmuhammad@gmail.com>
2023-11-13 08:29:48 +0700

    feat(view): add aztec color configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'tailwindcss'
-import {nextui} from "@nextui-org/react";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
@@ -9,7 +9,22 @@ const config: Config = {
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'aztec': {
+          DEFAULT: '#F5F8F7',
+          100: '#E0E7E6',
+          200: '#C0CFCC',
+          300: '#99AFAD',
+          400: '#738E8C',
+          500: '#597371',
+          600: '#465B5A',
+          700: '#3A4B4A',
+          800: '#313E3D',
+          900: '#2C3535',
+        },
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
