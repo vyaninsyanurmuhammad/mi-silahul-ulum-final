1f842e45e364249e0238b7ffe38eade119931861
UC8096 <vyaninsya.nurmuhammad@gmail.com>
2023-11-13 18:10:34 +0700

    refoctor(view): change about hero px-16 to px-64

--- app/components/about-card-home.tsx
+++ app/components/about-card-home.tsx
@@ -5,7 +5,7 @@ import { ArrowSmallRightIcon } from '@heroicons/react/24/outline'
 export default function AboutCardHome() {
     return (
         <>
-            <div className="flex justify-center w-full px-16 mt-16">
+            <div className="flex justify-center w-full px-64 mt-16">
                 <div className="flex flex-row gap-12 rounded-xl overflow-hidden bg-white w-full px-14 pt-14 shadow-xl">
                     <div className="bg-green-300">
                         <div className="w-60 h-60">Image</div>
