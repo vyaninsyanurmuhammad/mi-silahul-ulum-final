9beb80884de13b18e66bc97e2711ae326084f34d
UC8096 <vyaninsya.nurmuhammad@gmail.com>
2023-11-13 23:44:28 +0700

    refoctor(view): implement lotte animation at card

--- app/components/about-card-home.tsx
+++ app/components/about-card-home.tsx
@@ -1,24 +1,44 @@
+'use client'
+
 import { Button } from "@nextui-org/react"
 import React from 'react'
 import { ArrowSmallRightIcon } from '@heroicons/react/24/outline'
+import { Player } from "@lottiefiles/react-lottie-player"
 
 export default function AboutCardHome() {
     return (
         <>
-            <div className="flex justify-center w-full px-64 mt-16">
-                <div className="flex flex-row gap-12 rounded-xl overflow-hidden bg-white w-full px-14 pt-14 shadow-xl">
-                    <div className="w-60 h-60  bg-[url('/assets/img/about-model.png')] bg-cover"></div>
-                    <div className="flex flex-col justify-center gap-3">
-                        <div>
-                            <div className="text-xl font-semibold">Tentang Kami</div>
-                            <div className="text-2xl font-bold">Madrasah Ibtidaiyah Silahul Ulum</div>
-                        </div>
-                        <div>Descriptons</div>
-                        <Button className="w-fit rounded-full bg-aztec-500 text-white">Cek Sekarang <ArrowSmallRightIcon className="h-6 w-6 text-white"></ArrowSmallRightIcon> </Button>
-                    </div>
+            <section className="px-8 xl:px-64 mt-16">
+                <div className="relative rounded-xl overflow-hidden shadow-xl group">
+                    <div className="absolute w-full h-full bg-gray-300">
 
+                        <Player className="scale-[5.0] pl-[200px] group-hover:scale-[15.0] group-hover:pl-0 duration-1000"
+                            autoplay={true}
+                            loop={true}
+                            src="https://assets1.lottiefiles.com/packages/lf20_zd3udbkt.json"
+                            style={{ height: '1000px', width: '1000px', }}
+
+                        ></Player>
+
+                    </div>
+                    <div className="relative">
+                        <div className="flex justify-center w-full">
+                            <div className="flex flex-row gap-12 w-full px-14">
+                                <div className="w-60 h-60  bg-[url('/assets/img/about-model.png')] bg-cover"></div>
+                                <div className="flex flex-col justify-center gap-3">
+                                    <div>
+                                        <div className="text-xl font-semibold">Tentang Kami</div>
+                                        <div className="text-2xl font-bold">Madrasah Ibtidaiyah Silahul Ulum</div>
+                                    </div>
+                                    <div>Descriptons</div>
+                                    <Button className="w-fit rounded-full bg-aztec-500 text-white">Cek Sekarang <ArrowSmallRightIcon className="h-6 w-6 text-white"></ArrowSmallRightIcon> </Button>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </section>
 
         </>
     )
