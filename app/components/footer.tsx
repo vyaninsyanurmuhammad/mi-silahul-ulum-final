bf7e40c5b0f09d86ab9d4c4b0c598156f552e4b8
UC8096 <vyaninsya.nurmuhammad@gmail.com>
2023-11-16 10:12:30 +0700

    feat(view): add initial Footer at home page

---
+++ app/components/footer.tsx
@@ -0,0 +1,118 @@
+import { Link } from "@nextui-org/react";
+import React from 'react'
+
+export default function Footer() {
+    const navigations = [
+        {
+            judul: "tentang kami",
+            link: "#",
+            sub_navigation: [
+                {
+                    judul: "sejarah",
+                    link: "#"
+                },
+                {
+                    judul: "visi & misi",
+                    link: "#"
+                },
+                {
+                    judul: "sarana & prasarana",
+                    link: "#"
+                },
+                {
+                    judul: "kepala madrasah",
+                    link: "#"
+                },
+                {
+                    judul: "struktur madrasah",
+                    link: "#"
+                },
+                {
+                    judul: "pendidik",
+                    link: "#"
+                },
+            ]
+        },
+        {
+            judul: "pendidikan",
+            link: "#",
+            sub_navigation: [
+                {
+                    judul: "kurikulum",
+                    link: "#"
+                },
+                {
+                    judul: "prestasi madrasah",
+                    link: "#"
+                },
+                {
+                    judul: "seragam madrasah",
+                    link: "#"
+                },
+            ]
+        },
+        {
+            judul: "berita & acara",
+            link: "#",
+        },
+        {
+            judul: "galeri",
+            link: "#",
+        },
+        {
+            judul: "PPDB",
+            link: "#",
+        },
+        {
+            judul: "kontak",
+            link: "#",
+        },
+    ];
+
+    return (
+        <>
+            <footer className="w-full bg-aztec-500 h-min text-night px-8 xl:px-64 py-8">
+                <div className="flex flex-row w-full justify-center gap-6 h-min">
+                    <div className="flex flex-col gap-3 w-80">
+                        <div className="font-bold text-2xl">
+                            MI SILAHUL ULUM
+                        </div>
+                        <span className="text-night-100">
+                            RT.01/RW.01, Desa. Asempapan, Kec. Trangkil, Kabupaten Pati, Jawa Tengah 59153
+                        </span>
+                    </div>
+                    <div className="flex flex-row justify-center gap-8 w-full">
+                        {
+                            navigations.map((navigation) => (
+                                <div key={navigation.judul} className="flex flex-col gap-3 capitalize">
+                                    <div className="w-full">
+                                        <Link href={navigation.link} className="w-fit text-xl font-semibold text-night">{navigation.judul}</Link>
+                                    </div>
+                                    <div className="flex flex-col gap-2">
+                                        {
+                                            navigation.sub_navigation?.map((sub) => (
+                                                <div key={sub.judul} className="w-full">
+                                                    <Link href={sub.link} className="w-fit text-night-100">{sub.judul}</Link>
+                                                </div>
+                                            ))
+                                        }
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                    <div className="flex flex-col gap-3 w-80">
+                        <span className="text-night">
+                            Madrasah Ibtidaiyah Silahul Ulum berdiri dibawah naungan Yayaysan Silahul Ulum
+                        </span>
+                        <span className="text-night">
+                            © Hak Cipta 2023 - MI silahul ulum
+                        </span>
+                    </div>
+
+                </div>
+
+            </footer>
+        </>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@ import HeroHome from "./components/hero-home"
 import AboutCardHome from "./components/about-card-home"
 import NewsHome from "./components/news-home"
 import AchievementHome from "./components/achievement-home"
+import Footer from "./components/footer"
 
 export default function Home() {
   return (
     <>
       <NavigationBar></NavigationBar>
 
-      <div className="flex flex-col gap-16">
+      <div className="flex flex-col gap-24">
         <HeroHome></HeroHome>
         <AboutCardHome
           title="Madrasah Ibtidaiyah Silahul Ulum"
@@ -25,6 +26,7 @@ export default function Home() {
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
           imgModelSrc="/assets/img/ppdb-model.png">
         </AboutCardHome>
+        <Footer></Footer>
       </div>
 
     </>
